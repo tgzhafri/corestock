c3cacda86731f6bfb7dd796ad143c4fddff3bc24
Tengku Zhafri Tengku Abdul Rahman <tgzhafri@Tengkus-MacBook-Air.local>
2022-06-14 14:53:23 +0800

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,5 +29,8 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
