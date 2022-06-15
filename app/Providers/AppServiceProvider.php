18756f9b6bb599c37385cee3782c02d19c701cd8
Tengku Zhafri Tengku Abdul Rahman <tgzhafri@Tengkus-MacBook-Air.local>
2022-06-15 08:15:36 +0800

    test request https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 use \Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,6 +28,11 @@ class AppServiceProvider extends ServiceProvider
     {
         Paginator::useBootstrap();
 
-        \URL::forceScheme('https');
+        if (env('APP_ENV') !== 'local') {
+            $this->app['request']->server->set('HTTPS', true);
+            \URL::forceScheme('https');
+        }
+
+        Schema::defaultStringLength(191);
     }
 }
